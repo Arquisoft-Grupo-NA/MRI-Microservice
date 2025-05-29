be4dfb3cd6ee765e97f9708b0ef494ac0cec39ed
SebasmarR <sebastianmartinezarias@gmail.com>
2025-05-28 21:37:52 -0500

    aaa

--- app/templates.py
+++ app/templates.py
@@ -1,4 +1,5 @@
 from fastapi.templating import Jinja2Templates
 import os
 
-templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
